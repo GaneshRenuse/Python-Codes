9b31bb340ca29a59eebd4e5737a87e9da44c5567
coding sigma <92455602+GaneshRenuse@users.noreply.github.com>
2023-09-13 22:41:05 +0530

    Add files via upload

---
+++ Robospeaker.py
@@ -0,0 +1,27 @@
+import win32com.client as w
+import time
+
+speak = w.Dispatch("SAPI.SpVoice")
+
+print("--Welcome to Robo speaker chat bot--\n")
+text = "welcome to Robo speaker chat bot application, to communicate type your text"
+speak.Speak(text)
+
+text2 = input("Enter your text [available options (hello, how are you, tell me a quote)] :")
+
+if text2 == "hello":
+    text = "hii, how are you"
+    speak.Speak(text)
+elif text2 == "how are you":
+    text = "i am fine, what about you"
+    speak.Speak(text)
+elif text2 == "tell me a quote":
+    text = "sure, the quote is hardworking is a key to success"
+    speak.Speak(text)
+else:
+    text = "wrong input"
+    speak.Speak(text)
+
+time.sleep(3)
+text = "thanks for using robo speaker"
+speak.Speak(text)
