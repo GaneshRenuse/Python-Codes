e18a6891a170914ca14d5f57c25a48e8e995486f
coding sigma <ganesh21renuse3@gmail.com>
2023-10-30 20:42:29 +0530

    edited file

--- random_method.py
+++ random_method.py
@@ -1,8 +1,10 @@
 import random
 
 
+# d is the no of digits, or let's say length of the generated number.
 def my_random(d):
     return random.randrange(10**(d-1), 10**d)
 
 
+# print to test the output
 print(my_random(6))
